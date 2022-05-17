54d9a377a5929835751c048fbcb90f92615d6d17
* <irene.checa.chila@utelvt.edu.ec>
2022-05-17 11:02:28 -0500

    ChecaCamila-compara-segundaversion

--- CamilaCheca/ACTIVIDAD-B2/ChecaCamila-PuntoVenta.cpp
+++ CamilaCheca/ACTIVIDAD-B2/ChecaCamila-PuntoVenta.cpp
@@ -1,21 +1,54 @@
-#include <iostream>                     
-    using namespace std;
-int main()
-{
-        float d,g=0,i=0,x,vb,iva,vdes,vf;
-cout<<"ingrece la cantidad de producto que quiera sumar";
-cin>>d;  
-  g=g+1;
-i=i+d;
+
+   //===NOMBRE DEL PROGRAMA: PuntoVenta                                  //===AUTOR: Camila Checa 
+   //===ARCHIVO: PuntoVenta.cpp
+   //===FECHA DE ELABORACION:01-05-2022
+   ////===FECHA ULTIMA DE ACTUALIZACION: 13-05-2020
 
 
-{while (g<x)
-vb=i;                                
-iva*vb*0.12;
-vdes=vb*0.10;
-vf=vb+iva-vdes;                             
-cout<<"el valor final a pagar es:";
-  cout<<vf<<endl;
-}                                           
+
+#include<iostream>
+
+using namespace std;
+ int main() 
+{ 
+	
+	float PJ_x,PJ_c=0,PJ_a=0,PJ_n,PJ_vb,PJ_viva,PJ_vdesc,PJ_vf;
+      cout<<"ingrese cantidad de productos PJ_n :";
+      cin>>PJ_n;
+      do{
+  
+      cout<<"ingrese costo del producto PJ_x: ";
+      cin>>PJ_x;
+
+      PJ_c=PJ_c+1;
+      PJ_a=PJ_a+PJ_x;
+  
+
+      }while(PJ_c<PJ_n);
+  
+     PJ_vb=PJ_a;
+     PJ_viva=PJ_vb*0.12;
+     PJ_vdesc=PJ_vb*0.10;                             
+     PJ_vf=PJ_vb+PJ_viva-PJ_vdesc;   
+
+    cout<<"valor total es  :";  
+    cout<<PJ_vb<<endl;
+    cout<<"valor del iva es  :";
+    cout<<PJ_viva<<endl;
+    cout<<"valor del descuento  es :";                          
+    cout<<PJ_vdesc<<endl;                                                                                cout<<"total de productos son  :";    
+    cout<<PJ_n<<endl;               
+    cout<<"valor final a pagar es :";
+    cout<<PJ_vf<<endl;
+
+} 
+cout<<endl<<"//========================================="<<endl;
+cout<<"//===> Nombre del programa: PuntoVenta"<<endl;
+cout<<"//===> Archivo: PuntoVenta.cpp"<<endl;
+cout<<"//===> Autor: Camila Checa"<<endl;
+cout<<"//===> Fecha de elaboracion: 01-05-2022"<<endl;
+cout<<"//===> Fecha de actualizacion: 13-05-2022"<<endl;
+cout<<"//========================================="<<endl;
+
 return 0;
 }
--- CamilaCheca/ACTIVIDAD-B2/ChecaCamila-compara.cpp
+++ CamilaCheca/ACTIVIDAD-B2/ChecaCamila-compara.cpp
@@ -1,18 +1,38 @@
+
+// ================================================
+
+// ==> Nombre del programa: Compara
+
+// ==> Archivo : ChecaCamila-compara.cpp
+// ==>Autor: Camilla Checa
+// ==>Fecha de elaboración: 2022-04-2
+// ==>Fecha última actualización: 2022-05-17
+// ================================================ =
+
 #include<iostream>
 using namespace std;
 int main(){
 
-	float a,b;
-	cout<<"ingrse los valores de a y b "<<endl;
-	cin>>a>>b;
-	if(a==b)
-	{cout<<"a   es igual b";
+	float CCh_a,CCh_b;
+	cout<<"ingrse los valores de CCh_a y CCh_ b "<<endl;
+	cin>>CCh_a>>CCh_b;
+	if(CCh_a==CCh_b)
+	{cout<<"CCh_a  es igual CCh_b";
 	}
 	else{
-	if(a>b){
-	cout<<"b es mayor q a";
+cout<<endl<< " //========================================= ======= " <<endl;
+cout<< " //==> Nombre del programa: Punto de Venta " <<endl;
+cout<< " //==> Archivo : ChecaCamila-compara.cpp " <<endl;
+cout<< " //==>Autor: Camila Checa " <<endl;
+cout<< " //==>Fecha de elaboración: 2022-04-2 " <<endl;
+cout<< " //==>Fecha ultima actualización: 2022-05-17 " <<endl;
+cout<< " //============================================ ===== " <<endl;	
+
+
+	if(CCh_a>CCh_b){
+	cout<<"CCh_b es mayor q CCh_a";
 	}
-	else{cout<<"b es mayor que a";
+	else{cout<<"CCh_b es mayor que CCh_a";
 	}
 }
 return 0;	
