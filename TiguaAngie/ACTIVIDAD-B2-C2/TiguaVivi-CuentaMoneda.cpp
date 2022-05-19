eb501b5996260ae53abc1f6949a363e129195d5a
ViviTigua03 <viiviitigua@hotmail.com>
2022-05-18 23:44:23 -0500

    //Cuenta Moneda Actualizado

--- TiguaAngie/ACTIVIDAD-B2-C2/TiguaVivi-CuentaMoneda.cpp
+++ TiguaAngie/ACTIVIDAD-B2-C2/TiguaVivi-CuentaMoneda.cpp
@@ -1,30 +1,51 @@
+//================================================
+//==> Nombre del programa: Cuenta Moneda
+//==> Archivo : TiguaVivi-CuentaMoneda.cpp
+//==>Autor: Viviana Tigua
+//==>Fecha de elaboración: 2022-04-25
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
+
+
 #include<iostream> 
 using namespace std;
 int main()
 {          
-	int n, c=0, c1=0, c2=0;
-	float x, a=0, a1=0, a2=0, m=0.10;
+	int AR_n, AR_c=0, AR_c1=0, AR_c2=0;
+	float AR_x, AR_a=0, AR_a1=0, AR_a2=0, AR_m=0.10;
 	cout<<"Cantidad de monedas a ingresar : ";
-	cin>>n;
+	cin>>AR_n;
 	do{
-		cout<<"ingrese el valor de la moneda (0.10, 0.25) : ";
-		cin>>x;
-		c=c+1;
-		a=a+x;
-		if (x==m){
-			c1=c1+1;
-			a1=a1+x;
+		cout<<"Ingrese el valor de la moneda (0.10, 0.25) : ";
+		cin>>AR_x;
+		AR_c=AR_c+1;
+		AR_a=AR_a+AR_x;
+		if (AR_x==AR_m){
+			AR_c1=AR_c1+1;
+			AR_a1=AR_a1+AR_x;
 		}else{
-			c2=c2+1;
-			a2=a2+x; 
+			AR_c2=AR_c2+1;
+			AR_a2=AR_a2+AR_x; 
 		}
-}while(c<n);
+}while(AR_c<AR_n);
+
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Cuenta Moneda"<<endl;
+cout<<"//==> Archivo : TiguaVivi-CuentaMoneda"<<endl;
+cout<<"//==>Autor: Viviana Tigua"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-25"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-18"<<endl;
+cout<<"//================================================="<<endl;
+
+
+
 cout<<"El resultado fue:"<<endl;
-cout<<"Cantidad de monedas ingresadas : "<<c<<endl;
-cout<<"Cantidad total en dinero contado : "<<a<<endl;
-cout<<"Cantidad de monedas de 0.10c ingresadas : "<<c1<<endl;
-cout<<"Cantidad total en dinero de monedas de 0.10cc : "<<a1<<endl;
-cout<<"Cantidad de monedas de 0.25cc ingresadas : "<<c2<<endl;
-cout<<"Cantidad total en dinero de monedas de 0.25cc : "<<a2<<endl;
+cout<<"Cantidad de monedas ingresadas : "<<AR_c<<endl;
+cout<<"Cantidad total en dinero contado : "<<AR_a<<endl;
+cout<<"Cantidad de monedas de 0.10c ingresadas : "<<AR_c1<<endl;
+cout<<"Cantidad total en dinero de monedas de 0.10cc : "<<AR_a1<<endl;
+cout<<"Cantidad de monedas de 0.25cc ingresadas : "<<AR_c2<<endl;
+cout<<"Cantidad total en dinero de monedas de 0.25cc : "<<AR_a2<<endl;
 return 0;
 }
