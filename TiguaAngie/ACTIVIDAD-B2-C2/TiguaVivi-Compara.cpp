97e253275d9b6a7170da027a3289e712869f7369
ViviTigua03 <viiviitigua@hotmail.com>
2022-05-18 22:56:30 -0500

    //Comparación de dos números Actualizado

--- TiguaAngie/ACTIVIDAD-B2-C2/TiguaVivi-Compara.cpp
+++ TiguaAngie/ACTIVIDAD-B2-C2/TiguaVivi-Compara.cpp
@@ -1,17 +1,38 @@
+//================================================
+//==> Nombre del programa: Comparación de dos números
+//==> Archivo : TiguaVivi-Compara.cpp
+//==>Autor: Viviana Tigua
+//==>Fecha de elaboración: 2022-04-25
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main ()
 {
-        float t,r;
+        float AR_t,AR_r;
         cout<<"Ingrese el valor de T: ";
-        cin>>t;
+        cin>>AR_t;
         cout<<"Ingrese el valor de R: ";
-        cin>>r;
-        if (t==r){                                                    cout<<"El valor de T: "<<t<<" es igual a R: "<<r<<endl;
+        cin>>AR_r;
+        if (AR_t==AR_r){  
+	cout<<"El valor de T: "<<AR_t<<" es igual a R: "<<AR_r<<endl;
         }else{
-        if (t>r){                                                     cout<<"El valor de T: "<<t<<" es mayor a R: "<<r<<endl;
-        }else{                                                        cout<<"El valor de R: "<<r<<" es mayor a T: "<<t<<endl;
+        if (AR_t>AR_r){   
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Comparación de dos números"<<endl;
+cout<<"//==> Archivo : TiguaVivi-Compara.cpp"<<endl;
+cout<<"//==>Autor: Viviana Tigua"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-25"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-18"<<endl;
+cout<<"//================================================="<<endl;
+
+	cout<<"El valor de T: "<<AR_t<<" es mayor a R: "<<AR_r<<endl;
+        }else{  
+	cout<<"El valor de R: "<<AR_r<<" es mayor a T: "<<AR_t<<endl;
         }
+
         }
         return 0;
 }
